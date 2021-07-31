92503f6baead3245c4f55ae4b405e5ef4411e055
jesuspena91 <jesusp@seas.upenn.edu>
2021-07-31 12:42:11 -0500

    Update trace_txes.py

--- trace_txes.py
+++ trace_txes.py
@@ -39,7 +39,7 @@ class TXO:
     def from_tx_hash(cls,tx_hash,n=0):
         #YOUR CODE HERE
         tx = rpc_connection.getrawtransaction(tx_hash,True)
-        print(tx.amount)
+        print(tx)
         # self.tx_hash = tx_hash 
         # self.n = n
         # self.amount = amount
