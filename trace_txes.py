183abf835b2ac1a3177525fdcf27da440286c8b2
jesuspena91 <jesusp@seas.upenn.edu>
2021-07-31 16:09:08 -0500

    Update trace_txes.py

--- trace_txes.py
+++ trace_txes.py
@@ -52,7 +52,7 @@ class TXO:
                 addresses = scr['addresses']
                 owner_t = addresses[0]
             i=i+1
-        
+        print(owner_t)
         return clf( tx_hash=tx_hash_t, n=n_t, amount=amount_t,owner=owner_t, time=time_t )
 
     def get_inputs(self,d=1):
