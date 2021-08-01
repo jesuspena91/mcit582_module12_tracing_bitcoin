82c619090abe3dc3029fb27d4030ac65e3c90928
jesuspena91 <jesusp@seas.upenn.edu>
2021-08-01 15:48:55 -0500

    Update trace_txes.py

--- trace_txes.py
+++ trace_txes.py
@@ -58,7 +58,8 @@ class TXO:
 
     def get_inputs(self,d=1):
         #YOUR CODE HERE
-        tx = rpc_connection.getrawtransaction(tx_hash,True)
+        tx = rpc_connection.getrawtransaction(self.tx_hash,True)
+        print('fdsfsd')
         vins = tx['vin']
         
         if d == 1:
