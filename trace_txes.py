567b0168646e49fe2ed6a359b47c0476da6a8bb8
jesuspena91 <jesusp@seas.upenn.edu>
2021-08-01 12:22:53 -0500

    Update trace_txes.py

--- trace_txes.py
+++ trace_txes.py
@@ -39,21 +39,19 @@ class TXO:
     def from_tx_hash(cls,tx_hash,n=0):
         #YOUR CODE HERE
         tx = rpc_connection.getrawtransaction(tx_hash,True)
-        time_t = datetime.fromtimestamp(tx['time'])
+        time = datetime.fromtimestamp(tx['time'])
 
         vouts = tx['vout']
         i = 0
         for v in vouts:
             if i == n:
-                n_t = v['n']
-                amount_t = int(v['value']*100000000) #converting to Satoshi
-                print(amount_t)
+                amount = int(v['value']*100000000) #converting to Satoshi
                 scr = v['scriptPubKey']
                 addresses = scr['addresses']
-                owner_t = addresses[0]
+                owner = addresses[0]
             i=i+1
-        print(1)
-        return cls( tx_hash=tx_hash, n=n, amount=amount_t,owner=owner_t, time=time_t )
+        
+        return cls( tx_hash=tx_hash, n=n, amount=amount,owner=owner, time=time )
 
     def get_inputs(self,d=1):
         #YOUR CODE HERE
